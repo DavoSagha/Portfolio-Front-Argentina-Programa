9504366bf995ffa9e5e436ec4f7af0a739ee8bba
DavoSagha <saghatelyandsm@gmail.com>
2022-10-26 19:49:50 -0300

    portfolio front

---
+++ main.js
@@ -0,0 +1,10 @@
+/* var header = document.querySelector("#header");
+
+window.addEventListener("scroll", ()=>{
+    var scroll = window.scrollY
+    if(scroll>10){
+        header.style.backgroundColor = "#272727"
+    }else{
+        header.style.backgroundColor = "transparent"
+    }
+}) */
